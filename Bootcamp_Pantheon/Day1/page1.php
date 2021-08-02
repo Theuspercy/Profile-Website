af550c92013e710f685d60ad80aabd71c8d6a85c
Wasselay <70000658+Theuspercy@users.noreply.github.com>
2021-08-02 21:10:09 +0800

    Add files via upload

---
+++ Bootcamp_Pantheon/Day1/page1.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Justin Barcos</title>
+    <link rel="stylesheet" type="text/css" href="page1.css" />
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <div class="firstpic">
+      <h1 class="name">I'm Justin Barcos
+      <br>
+      <br>
+      <p class="introstatement">“Seeing stars at night gives me calm and peace in my mind”</p>
+    </div>
+    <br>
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="secondpic col-5">
+            <img src="https://scontent.fmnl17-4.fna.fbcdn.net/v/t1.6435-9/153820437_2567533146876292_7103342681663667133_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeF38XvoR5ifLM91dvU4BxdJM9kf7RZexf8z2R_tFl7F_9pDIAnAFmlVkCNuF89H47k3lpSwm9YcI07WLWMnQtnk&_nc_ohc=JN3Xg8oG9AgAX_Mu1e8&_nc_ht=scontent.fmnl17-4.fna&oh=6037378a5bb235010b28d575ffed269e&oe=612EB1C9" alt="justinpic"class="img-fluid rounded-circle" height=500 width=500>
+        </div>
+        <div class="col-7 Myinfos">
+          <p>Hello Everyone!</p>
+          <p>I'm <b>Justin Angelo S. Barcos</b></p>
+          <p>Studying at University of Makati and now taking 2nd Year College as Computer Science Student
+          <p>Born in November 3, 2001 which is now 19 years old</p>
+          <p>I like to listen to old song</p>
+          <p>My favorite foods are Pares, Kadiyos, Bulalo and Adobo</p>
+          <p>My dream is to explore the space and yes to be an Astronaut</p>
+          <p>I like playing games specially Call of Duty Mobile</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
